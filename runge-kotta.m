dc79de1a2c6032d60aba4c71104e2559510b353d
Carlos Barajas <12barajasc@gmail.com>
2015-04-16 15:53:52 -0400

    Comments added for later installments

---
+++ runge-kotta.m
@@ -0,0 +1,14 @@
+function runkot(init_x, init_y, fin_x, step)
+% Symolic Equation
+syms f(x,y)
+
+% Acquire the differential equation
+f(x, y) = sym (input('y'' = ', 's'))
+
+% Set up k1 -> k4
+
+% Calculate using step values
+
+% Plot the approximations
+
+% Plot the vector field
