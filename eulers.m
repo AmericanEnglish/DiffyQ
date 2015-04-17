86a65b0b959d54f1334f9aa53f996b5841c1af25
Carlos Barajas <12barajasc@gmail.com>
2015-04-16 20:53:51 -0400

    Changed the vector field plotting and some feedback msgs

--- eulers.m
+++ eulers.m
@@ -4,9 +4,9 @@ syms f(x,y)
 
 % Then actually setup the equation in question
 f(x,y) = sym (input('y'' = ', 's'))
-cla
+clf
 % Graph it
-hold
+hold ;
 x1 = [init_x];
 y1 = [init_y];
 counter = 0;
@@ -16,19 +16,21 @@ while counter < (fin_x - init_x) / step
     y1(counter + 1) = f(x1(counter), y1(counter)) * step + y1(counter);
 end
 plot(x1, y1)
-hold off
+hold off ;
 
 % Vector Field
 % Divide the step by 2 for a more accurate vector field
-hold
-for x0 = [init_x:step/2:fin_x]
+% Draws a Vector Field
+disp('Creating Vector Field . . .')
+hold ;
+for x1 = [init_x:step/2:fin_x];
     % Use the min and max of y1 to determine best range
-    for y0 = [min(y1):step/2:max(y1)]
-        x_temp = [-step/2, step/2]*.25;
-        y_temp = f(x0, y0) * x_temp;
-        len = sqrt(x_temp(1)^2 + x_temp(2) ^ 2);
-        plot(x_temp+x0, y_temp+y0);
+    for y1 = [min(y0):step/2:max(y0)];
+        x_temp =[-.25,.25]*.25;
+        y_temp = f(x1, y1) * x_temp ;
+        lg = sqrt((x_temp(2)-x_temp(1)) ^ 2 + (y_temp(2) - y_temp(1)) ^ 2) * 1/step;
+        plot(x1 + x_temp/ lg , y1 + y_temp / lg, 'black');
     end
 end
-hold off
+hold off;
 disp('Finished Graphing.')
--- ieulers.m
+++ ieulers.m
@@ -4,32 +4,33 @@ syms f(x, y)
 
 % Prompt for the equation
 f(x, y) = sym (input('y'' = ', 's'))
-cla
+clf
 
 % Calculate Improved Eulers
 x0 = [init_x];
 y0 = [init_y];
 counter = 0;
+disp('Calculating Values . . .')
 while counter < (fin_x - init_x) / step;
     counter = counter + 1;
     x0(counter + 1) = x0(counter) + step;
     y0(counter + 1) = y0(counter) + step / 2 * (f(x0(counter), y0(counter)) + f(x0(counter + 1), y0(counter) + step * f(x0(counter), y0(counter))));
 end
-hold
+hold;
 plot(x0, y0)
-hold off
+hold off;
 
 % Draws a Vector Field
-hold
-for x1 = [init_x:step/2:fin_x]
+disp('Creating Vector Field . . .')
+hold ;
+for x1 = [init_x:step/2:fin_x];
     % Use the min and max of y1 to determine best range
-    for y1 = [min(y0):step/2:max(y0)]
-        x_temp = [-step/2, step/2]*.25;
-        y_temp = f(x1, y1) * x_temp;
-        lenx = sqrt(x_temp(1)^ 2 + x_temp(2) ^ 2);
-        leny = sqrt(y_temp(1)^ 2 + y_temp(2) ^ 2);
-        plot(x1 + x_temp, y1 + y_temp);
+    for y1 = [min(y0):step/2:max(y0)];
+        x_temp =[-.25,.25]*.25;
+        y_temp = f(x1, y1) * x_temp ;
+        lg = sqrt((x_temp(2)-x_temp(1)) ^ 2 + (y_temp(2) - y_temp(1)) ^ 2) * 1/step;
+        plot(x1 + x_temp/ lg , y1 + y_temp / lg, 'black');
     end
 end
-hold off
+hold off;
 disp('Finished Graphing.')
