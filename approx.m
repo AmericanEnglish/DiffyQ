2222956788a301c1dafa3311830333dbe4fa8573
Carlos Barajas <12barajasc@gmail.com>
2015-04-17 22:32:32 -0400

    Optimizing Data

--- approx.m
+++ approx.m
@@ -3,18 +3,16 @@ function approx(init_x, init_y, fin_x, h)
 syms f(x,y)
 f(x,y) = sym (input('y'' = ', 's'))
 clf
+% Eulers
 disp('Calculating Eulers . . .')
-hold ;
 x0 = [init_x];
-y0 = [init_y];
+y1 = [init_y];
 counter = 0;
 while counter < (fin_x - init_x) / h
     counter =  counter + 1;
     x0(counter + 1) = x0(counter) + h;
-    y0(counter + 1) = f(x0(counter), y0(counter)) * h + y0(counter);
+    y1(counter + 1) = f(x0(counter), y1(counter)) * h + y1(counter);
 end
-plot(x0, y0)
-hold off ;
 
 % Vector Field since Euler has the worst approx
 disp('Creating Vector Field . . .')
@@ -32,39 +30,28 @@ hold off;
 
 
 % Improved Euelers
-x0 = [init_x];
-y0 = [init_y];
+y2 = [init_y];
 counter = 0;
 disp('Calculating Improved Eulers . . .')
 while counter < (fin_x - init_x) / h;
     counter = counter + 1;
-    x0(counter + 1) = x0(counter) + h;
-    y0(counter + 1) = y0(counter) + h / 2 * (f(x0(counter), y0(counter)) + f(x0(counter + 1), y0(counter) + h * f(x0(counter), y0(counter))));
+    y2(counter + 1) = y2(counter) + h / 2 * (f(x0(counter), y2(counter)) + f(x0(counter + 1), y2(counter) + h * f(x0(counter), y2(counter))));
 end
-hold;
-plot(x0, y0, 'cyan--*')
-hold off;
 
 % Taylors Series
 disp('Calculating Taylor Differentials . . .')
-hold;
 syms f2(x,y) f3(x,y) f4(x,y)
 f2(x,y) = diff(f(x,y), x) + diff(f(x,y), y) * f(x,y);
 f3(x,y) = diff(f2(x,y), x) + diff(f2(x,y), y) * f2(x,y);
 f4(x,y) = diff(f3(x,y), x) + diff(f3(x,y), y) * f3(x,y);
 
-x0 = [init_x];
-y0 = [init_y];
+y3 = [init_y];
 counter = 0;
 while counter < (fin_x - init_x) / h
     counter = counter + 1;
-    x0(counter + 1) = x0(counter) + h;
-    y0(counter + 1) = y0(counter) + h* f(x0(counter), y0(counter)) + (h^2)/2 * f2(x0(counter), y0(counter)) + (h^3)/factorial(3 ) * f3(x0(counter), y0(counter)) + (h^4)/factorial(4) * f4(x0(counter), y0(counter));
+    y3(counter + 1) = y3(counter) + h* f(x0(counter), y3(counter)) + (h^2)/2 * f2(x0(counter), y3(counter)) + (h^3)/factorial(3 ) * f3(x0(counter), y3(counter)) + (h^4)/factorial(4) * f4(x0(counter), y3(counter));
 end
-plot(x0, y0, 'm:');
-hold off;
 
-hold;
 disp('Calculating Runge-Kotta Approximations . . .')
 
 k1(x,y) = h * f(x, y);
@@ -74,15 +61,20 @@ k4(x,y) = h * f(x + h, y + k3);
 
 % Calculate using h values
 x0 = [init_x];
-y0 = [init_y];
+RungeKotta = [init_y];
 counter = 0;
 % Plot the approximations
 while counter < (fin_x - init_x) / h;
     counter = counter + 1;
     x0(counter + 1) = x0(counter) + h;
-    y0(counter + 1) = y0(counter) + 1/6 * (k1(x0(counter), y0(counter)) + 2*k2(x0(counter), y0(counter)) + 2*k3(x0(counter), y0(counter)) + k4(x0(counter), y0(counter)));
+    RungeKotta(counter + 1) = y4(counter) + 1/6 * (k1(x0(counter), y4(counter)) + 2*k2(x0(counter), y4(counter)) + 2*k3(x0(counter), y4(counter)) + k4(x0(counter), y4(counter)));
 end
-plot(x0, y0, 'green--')
-hold off;
 
+% Time to solve the function!
+syms z(t)
+z(t) = dsolve(diff(z) == f(t, z), z(init_x) == init_y)
+
+hold
+plot(x0, z(x0), x0, y1, x0, y2, x0, y3, x0, RungeKotta)
+hold off;
 disp('Finished Graphing.')
